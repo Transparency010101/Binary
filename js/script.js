213beb44867c49d584b3dcbd5178b1dee1ed1427
Transparency010101 <ossys1010110@gmail.com>
2021-08-13 18:37:32 +0300

    add form

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
+// ! Забей
+const hi = 'Hello world!';
 function hi() {
-    console.log('Hello world!');
+    console.log(hi);
 }
 hi();
-
-
+function read_more() {
+    console.log('Read more');
+}
